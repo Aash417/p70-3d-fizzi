08256e6582e5c71206ccf9d39d21a3edce82f621
aashish_k <aashishkathat00@gmail.com>
2024-09-28 16:57:47 +0530

    skydiving can animation added

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -4,64 +4,64 @@ import type * as prismic from "@prismicio/client";
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
-type PageDocumentDataSlicesSlice = HeroSlice;
+type PageDocumentDataSlicesSlice = SkyDiveSlice | HeroSlice;
 
 /**
  * Content for Page documents
  */
 interface PageDocumentData {
-  /**
-   * Title field in *Page*
-   *
-   * - **Field Type**: Title
-   * - **Placeholder**: *None*
-   * - **API ID Path**: page.title
-   * - **Tab**: Main
-   * - **Documentation**: https://prismic.io/docs/field#rich-text-title
-   */
-  title: prismic.TitleField;
+   /**
+    * Title field in *Page*
+    *
+    * - **Field Type**: Title
+    * - **Placeholder**: *None*
+    * - **API ID Path**: page.title
+    * - **Tab**: Main
+    * - **Documentation**: https://prismic.io/docs/field#rich-text-title
+    */
+   title: prismic.TitleField;
 
-  /**
-   * Slice Zone field in *Page*
-   *
-   * - **Field Type**: Slice Zone
-   * - **Placeholder**: *None*
-   * - **API ID Path**: page.slices[]
-   * - **Tab**: Main
-   * - **Documentation**: https://prismic.io/docs/field#slices
-   */
-  slices: prismic.SliceZone<PageDocumentDataSlicesSlice> /**
-   * Meta Title field in *Page*
-   *
-   * - **Field Type**: Text
-   * - **Placeholder**: A title of the page used for social media and search engines
-   * - **API ID Path**: page.meta_title
-   * - **Tab**: SEO & Metadata
-   * - **Documentation**: https://prismic.io/docs/field#key-text
-   */;
-  meta_title: prismic.KeyTextField;
+   /**
+    * Slice Zone field in *Page*
+    *
+    * - **Field Type**: Slice Zone
+    * - **Placeholder**: *None*
+    * - **API ID Path**: page.slices[]
+    * - **Tab**: Main
+    * - **Documentation**: https://prismic.io/docs/field#slices
+    */
+   slices: prismic.SliceZone<PageDocumentDataSlicesSlice> /**
+    * Meta Title field in *Page*
+    *
+    * - **Field Type**: Text
+    * - **Placeholder**: A title of the page used for social media and search engines
+    * - **API ID Path**: page.meta_title
+    * - **Tab**: SEO & Metadata
+    * - **Documentation**: https://prismic.io/docs/field#key-text
+    */;
+   meta_title: prismic.KeyTextField;
 
-  /**
-   * Meta Description field in *Page*
-   *
-   * - **Field Type**: Text
-   * - **Placeholder**: A brief summary of the page
-   * - **API ID Path**: page.meta_description
-   * - **Tab**: SEO & Metadata
-   * - **Documentation**: https://prismic.io/docs/field#key-text
-   */
-  meta_description: prismic.KeyTextField;
+   /**
+    * Meta Description field in *Page*
+    *
+    * - **Field Type**: Text
+    * - **Placeholder**: A brief summary of the page
+    * - **API ID Path**: page.meta_description
+    * - **Tab**: SEO & Metadata
+    * - **Documentation**: https://prismic.io/docs/field#key-text
+    */
+   meta_description: prismic.KeyTextField;
 
-  /**
-   * Meta Image field in *Page*
-   *
-   * - **Field Type**: Image
-   * - **Placeholder**: *None*
-   * - **API ID Path**: page.meta_image
-   * - **Tab**: SEO & Metadata
-   * - **Documentation**: https://prismic.io/docs/field#image
-   */
-  meta_image: prismic.ImageField<never>;
+   /**
+    * Meta Image field in *Page*
+    *
+    * - **Field Type**: Image
+    * - **Placeholder**: *None*
+    * - **API ID Path**: page.meta_image
+    * - **Tab**: SEO & Metadata
+    * - **Documentation**: https://prismic.io/docs/field#image
+    */
+   meta_image: prismic.ImageField<never>;
 }
 
 /**
@@ -74,7 +74,7 @@ interface PageDocumentData {
  * @typeParam Lang - Language API ID of the document.
  */
 export type PageDocument<Lang extends string = string> =
-  prismic.PrismicDocumentWithUID<Simplify<PageDocumentData>, "page", Lang>;
+   prismic.PrismicDocumentWithUID<Simplify<PageDocumentData>, "page", Lang>;
 
 export type AllDocumentTypes = PageDocument;
 
@@ -82,85 +82,85 @@ export type AllDocumentTypes = PageDocument;
  * Primary content in *Hero → Default → Primary*
  */
 export interface HeroSliceDefaultPrimary {
-  /**
-   * Heading field in *Hero → Default → Primary*
-   *
-   * - **Field Type**: Title
-   * - **Placeholder**: *None*
-   * - **API ID Path**: hero.default.primary.heading
-   * - **Documentation**: https://prismic.io/docs/field#rich-text-title
-   */
-  heading: prismic.TitleField;
+   /**
+    * Heading field in *Hero → Default → Primary*
+    *
+    * - **Field Type**: Title
+    * - **Placeholder**: *None*
+    * - **API ID Path**: hero.default.primary.heading
+    * - **Documentation**: https://prismic.io/docs/field#rich-text-title
+    */
+   heading: prismic.TitleField;
 
-  /**
-   * SubHeading field in *Hero → Default → Primary*
-   *
-   * - **Field Type**: Rich Text
-   * - **Placeholder**: *None*
-   * - **API ID Path**: hero.default.primary.subheading
-   * - **Documentation**: https://prismic.io/docs/field#rich-text-title
-   */
-  subheading: prismic.RichTextField;
+   /**
+    * SubHeading field in *Hero → Default → Primary*
+    *
+    * - **Field Type**: Rich Text
+    * - **Placeholder**: *None*
+    * - **API ID Path**: hero.default.primary.subheading
+    * - **Documentation**: https://prismic.io/docs/field#rich-text-title
+    */
+   subheading: prismic.RichTextField;
 
-  /**
-   * Body field in *Hero → Default → Primary*
-   *
-   * - **Field Type**: Rich Text
-   * - **Placeholder**: *None*
-   * - **API ID Path**: hero.default.primary.body
-   * - **Documentation**: https://prismic.io/docs/field#rich-text-title
-   */
-  body: prismic.RichTextField;
+   /**
+    * Body field in *Hero → Default → Primary*
+    *
+    * - **Field Type**: Rich Text
+    * - **Placeholder**: *None*
+    * - **API ID Path**: hero.default.primary.body
+    * - **Documentation**: https://prismic.io/docs/field#rich-text-title
+    */
+   body: prismic.RichTextField;
 
-  /**
-   * Button Text field in *Hero → Default → Primary*
-   *
-   * - **Field Type**: Text
-   * - **Placeholder**: *None*
-   * - **API ID Path**: hero.default.primary.button_text
-   * - **Documentation**: https://prismic.io/docs/field#key-text
-   */
-  button_text: prismic.KeyTextField;
+   /**
+    * Button Text field in *Hero → Default → Primary*
+    *
+    * - **Field Type**: Text
+    * - **Placeholder**: *None*
+    * - **API ID Path**: hero.default.primary.button_text
+    * - **Documentation**: https://prismic.io/docs/field#key-text
+    */
+   button_text: prismic.KeyTextField;
 
-  /**
-   * Button Link field in *Hero → Default → Primary*
-   *
-   * - **Field Type**: Link
-   * - **Placeholder**: *None*
-   * - **API ID Path**: hero.default.primary.button_link
-   * - **Documentation**: https://prismic.io/docs/field#link-content-relationship
-   */
-  button_link: prismic.LinkField;
+   /**
+    * Button Link field in *Hero → Default → Primary*
+    *
+    * - **Field Type**: Link
+    * - **Placeholder**: *None*
+    * - **API ID Path**: hero.default.primary.button_link
+    * - **Documentation**: https://prismic.io/docs/field#link-content-relationship
+    */
+   button_link: prismic.LinkField;
 
-  /**
-   * Cans Image field in *Hero → Default → Primary*
-   *
-   * - **Field Type**: Image
-   * - **Placeholder**: *None*
-   * - **API ID Path**: hero.default.primary.cans_image
-   * - **Documentation**: https://prismic.io/docs/field#image
-   */
-  cans_image: prismic.ImageField<never>;
+   /**
+    * Cans Image field in *Hero → Default → Primary*
+    *
+    * - **Field Type**: Image
+    * - **Placeholder**: *None*
+    * - **API ID Path**: hero.default.primary.cans_image
+    * - **Documentation**: https://prismic.io/docs/field#image
+    */
+   cans_image: prismic.ImageField<never>;
 
-  /**
-   * Second Heading field in *Hero → Default → Primary*
-   *
-   * - **Field Type**: Title
-   * - **Placeholder**: *None*
-   * - **API ID Path**: hero.default.primary.second_heading
-   * - **Documentation**: https://prismic.io/docs/field#rich-text-title
-   */
-  second_heading: prismic.TitleField;
+   /**
+    * Second Heading field in *Hero → Default → Primary*
+    *
+    * - **Field Type**: Title
+    * - **Placeholder**: *None*
+    * - **API ID Path**: hero.default.primary.second_heading
+    * - **Documentation**: https://prismic.io/docs/field#rich-text-title
+    */
+   second_heading: prismic.TitleField;
 
-  /**
-   * Second Body field in *Hero → Default → Primary*
-   *
-   * - **Field Type**: Rich Text
-   * - **Placeholder**: *None*
-   * - **API ID Path**: hero.default.primary.second_body
-   * - **Documentation**: https://prismic.io/docs/field#rich-text-title
-   */
-  second_body: prismic.RichTextField;
+   /**
+    * Second Body field in *Hero → Default → Primary*
+    *
+    * - **Field Type**: Rich Text
+    * - **Placeholder**: *None*
+    * - **API ID Path**: hero.default.primary.second_body
+    * - **Documentation**: https://prismic.io/docs/field#rich-text-title
+    */
+   second_body: prismic.RichTextField;
 }
 
 /**
@@ -171,9 +171,9 @@ export interface HeroSliceDefaultPrimary {
  * - **Documentation**: https://prismic.io/docs/slice
  */
 export type HeroSliceDefault = prismic.SharedSliceVariation<
-  "default",
-  Simplify<HeroSliceDefaultPrimary>,
-  never
+   "default",
+   Simplify<HeroSliceDefaultPrimary>,
+   never
 >;
 
 /**
@@ -190,24 +190,89 @@ type HeroSliceVariation = HeroSliceDefault;
  */
 export type HeroSlice = prismic.SharedSlice<"hero", HeroSliceVariation>;
 
-declare module "@prismicio/client" {
-  interface CreateClient {
-    (
-      repositoryNameOrEndpoint: string,
-      options?: prismic.ClientConfig,
-    ): prismic.Client<AllDocumentTypes>;
-  }
+/**
+ * Primary content in *SkyDive → Default → Primary*
+ */
+export interface SkyDiveSliceDefaultPrimary {
+   /**
+    * Sentence field in *SkyDive → Default → Primary*
+    *
+    * - **Field Type**: Text
+    * - **Placeholder**: *None*
+    * - **API ID Path**: sky_dive.default.primary.sentence
+    * - **Documentation**: https://prismic.io/docs/field#key-text
+    */
+   sentence: prismic.KeyTextField;
 
-  namespace Content {
-    export type {
-      PageDocument,
-      PageDocumentData,
-      PageDocumentDataSlicesSlice,
-      AllDocumentTypes,
-      HeroSlice,
-      HeroSliceDefaultPrimary,
-      HeroSliceVariation,
-      HeroSliceDefault,
-    };
-  }
+   /**
+    * Flavor field in *SkyDive → Default → Primary*
+    *
+    * - **Field Type**: Select
+    * - **Placeholder**: *None*
+    * - **API ID Path**: sky_dive.default.primary.flavor
+    * - **Documentation**: https://prismic.io/docs/field#select
+    */
+   flavor: prismic.SelectField<
+      | "lemonLime"
+      | "grape"
+      | "blackCherry"
+      | "strawberryLemonade"
+      | "watermelon"
+   >;
+}
+
+/**
+ * Default variation for SkyDive Slice
+ *
+ * - **API ID**: `default`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type SkyDiveSliceDefault = prismic.SharedSliceVariation<
+   "default",
+   Simplify<SkyDiveSliceDefaultPrimary>,
+   never
+>;
+
+/**
+ * Slice variation for *SkyDive*
+ */
+type SkyDiveSliceVariation = SkyDiveSliceDefault;
+
+/**
+ * SkyDive Shared Slice
+ *
+ * - **API ID**: `sky_dive`
+ * - **Description**: SkyDive
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type SkyDiveSlice = prismic.SharedSlice<
+   "sky_dive",
+   SkyDiveSliceVariation
+>;
+
+declare module "@prismicio/client" {
+   interface CreateClient {
+      (
+         repositoryNameOrEndpoint: string,
+         options?: prismic.ClientConfig,
+      ): prismic.Client<AllDocumentTypes>;
+   }
+
+   namespace Content {
+      export type {
+         PageDocument,
+         PageDocumentData,
+         PageDocumentDataSlicesSlice,
+         AllDocumentTypes,
+         HeroSlice,
+         HeroSliceDefaultPrimary,
+         HeroSliceVariation,
+         HeroSliceDefault,
+         SkyDiveSlice,
+         SkyDiveSliceDefaultPrimary,
+         SkyDiveSliceVariation,
+         SkyDiveSliceDefault,
+      };
+   }
 }
